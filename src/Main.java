75d63178919c68582c4ad98b75b00531dbb2c302
Antonov333 <antonovsergei333@yahoo.com>
2023-04-28 13:56:57 +0300

    Problem solved

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,20 @@ public class Main {
         System.out.println("Hello world!\n\nCourse2\n" +
                 "topic: OOP\nExtra problem 2-2-4 \"Matrix\"");
 
-        Matrix m = new Matrix(2, 2);
+        int lines = 3, rows = 4;
 
-        System.out.println("m = " + m);
+        Matrix a = new Matrix(lines, rows);
+        Matrix b = new Matrix(lines, rows);
+        Matrix c = new Matrix(lines, rows);
+        a.fill(1);
+        b.fill(1);
+        b.multiplyByNumber(2);
+        a.printMatrix();
+        b.printMatrix();
+
+        c = Matrix.sum(a, b);
+
+        c.printMatrix();
 
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -9,6 +9,62 @@ public class Matrix {
     }
 
     public Matrix(int numberOfLines, int numberOfRows) {
+        this.numberOfLines = numberOfLines;
+        this.numberOfRows = numberOfRows;
         this.matrix = new double[numberOfLines][numberOfRows];
+    }
+
+    public double getElement(int line, int row) {
+        return matrix[line - 1][row - 1];
+    }
+
+    public void setElement(int line, int row, double value) {
+        matrix[line - 1][row - 1] = value;
+    }
+
+    public void printMatrix() {
+        System.out.println("Printing matrix of " + numberOfLines + " lines and " + numberOfRows + " rows");
+        for (int i = 0; i <= numberOfLines - 1; i++) {
+            for (int j = 0; j <= numberOfRows - 1; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println("");
+        }
+    }
+
+    public void fill(double value) {
+        for (int i = 0; i <= numberOfLines - 1; i++) {
+            for (int j = 0; j <= numberOfRows - 1; j++) {
+                matrix[i][j] = value;
+            }
+        }
+
+    }
+
+    public void multiplyByNumber(double value) {
+        for (int i = 0; i <= numberOfLines - 1; i++) {
+            for (int j = 0; j <= numberOfRows - 1; j++) {
+                matrix[i][j] = matrix[i][j] * value;
+            }
+        }
+    }
+
+    public static Matrix sum(Matrix a, Matrix b) {
+        int lines = a.numberOfLines,
+                rows = a.numberOfRows;
+        if (rows != b.numberOfRows || lines != b.numberOfLines) {
+            System.out.println("different dimensions");
+            return null;
+        }
+
+        Matrix result = new Matrix(lines, rows);
+        for (int i = 1; i <= lines; i++) {
+
+
+            for (int j = 1; j <= rows; j++) {
+                result.setElement(i, j, a.getElement(i, j) + b.getElement(i, j));
+            }
+        }
+        return result;
     }
 }
