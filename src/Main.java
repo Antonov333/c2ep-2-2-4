fddde27982fad96cf47b1747e13a96bbc7f25321
Antonov333 <antonovsergei333@yahoo.com>
2023-04-28 12:44:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n\nCourse2\n" +
+                "topic: OOP\nExtra problem 2-2-4 \"Matrix\"");
+    }
+}
